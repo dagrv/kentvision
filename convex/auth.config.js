41b1b5c9a3c992099d7e2c3233d4de0eedf73d48
djamal <rust.91@icloud.com>
2024-01-31 23:37:57 +0100

    Convex + Clerk

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers : [
+        {
+            domain: "https://intimate-lamb-18.clerk.accounts.dev",
+            applicationID: "convex"
+        },
+    ]
+};
---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+
+interface ConvexClientProvideProps {
+    children: React.ReactNode
+};
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+const convex = new ConvexReactClient(convexUrl);
+
+export const ConvexClientProvider = ({
+    children
+}: ConvexClientProvideProps) => {
+    return (
+        <ClerkProvider>
+            <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+                <Authenticated>
+                    {children}
+                </Authenticated>
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    );
+};
+
