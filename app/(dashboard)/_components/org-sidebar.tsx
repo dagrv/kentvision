7d92c788c9da57077d3008ecc4f1e2984de17bb8
djamal <rust.91@icloud.com>
2024-02-01 22:23:03 +0100

    Restrictions

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,9 +1,68 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+import { LayoutDashboard, Star } from "lucide-react";
+import { Poppins } from "next/font/google";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { useSearchParams } from 'next/navigation';
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"]
+})
+
 export const OrgSidebar = () => {
+    const searchParams = useSearchParams();
+    const favorites = searchParams.get("favorites")
+
     return (
-        <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-yellow-600">
-            Org Sidebar
+        <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
+            <Link href="/">
+                <Image src="/logo.svg" alt="Logo" height={60} width={100} />
+            </Link>
+
+            <OrganizationSwitcher hidePersonal 
+                appearance={{
+                    elements: {
+                        rootBox: {
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            width: "100%"
+                        },
+                        organizationSwitcherTrigger: {
+                            padding: "6px",
+                            width: "100%",
+                            borderRadius: "8px",
+                            border: "1px solid #E5EBEB",
+                            justifyContent: "space-between",
+                            backgroundColor: "white"
+                        }
+                    }
+                }} />
+
+            <div className="space-y-1 w-full">
+                <Button asChild size="lg" className="font-normal justify-start px-2 w-full bg-red-100 text-red-800">
+                    <Link href="/">
+                        <LayoutDashboard className="h-4 w-4 mr-2 text-red-600" />All Boards
+                    </Link>
+                </Button>
+
+                <Button asChild size="lg" className="font-normal justify-start px-2 w-full bg-yellow-100 text-yellow-800">
+                    <Link href={{ pathname: "/", query: { favorites: true }}}>
+                        <Star className="h-4 w-4 mr-2 text-yellow-800" />Favorites
+                    </Link>
+                </Button>
+
+                <Button asChild size="lg" className="font-normal justify-start px-2 w-full bg-green-100 text-green-800">
+                    <Link href={{ pathname: "/", query: { favorites: true }}}>
+                        <Star className="h-4 w-4 mr-2 text-green-800" />Created
+                    </Link>
+                </Button>
+            </div>
+
         </div>
     )
 }
---
+++ components/invite-button.tsx
@@ -0,0 +1,22 @@
+import { OrganizationProfile } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+export const InviteButton = () => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline">
+                    <Plus />
+                    Invite
+                </Button>
+            </DialogTrigger>
+
+            <DialogContent className="p-0 bg-transparent border-none max-w-[880px]">
+                <OrganizationProfile />
+            </DialogContent>
+        </Dialog>
+    )
+}
