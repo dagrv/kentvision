6ce38bec98a673d62b36c4924f73f557455bcd7a
dagrv <rust.91@icloud.com>
2024-02-11 00:59:15 +0100

    Canvas & Toolbar

---
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -0,0 +1,28 @@
+export const Toolbar = () => {
+    return (
+        <div className="absolute top-[70%] -translate-y-[70%] left-2 flex flex-col gap-y-4">
+            <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-lg">
+                <div>
+                    Pencil
+                </div>
+
+                <div>
+                    Square
+                </div>
+
+                <div>
+                    Circle
+                </div>
+
+                <div>
+                    Ellipsis
+                </div>
+            </div>
+
+            <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-lg">
+                <div>Undo</div>
+                <div>Redo</div>
+            </div>
+        </div>
+    )
+}
